4d6479ef315d598cac48fe239ec99b579f3a255b
Lawrence Ma <lawrencema415@gmail.com>
2025-10-16 16:56:41 -0700

    Adjust width for mobile view on categories

--- app/components/Content/index.tsx
+++ app/components/Content/index.tsx
@@ -2,16 +2,23 @@ import { List } from '@mantine/core';
 import SideMenu from './SideMenu';
 import { highlightText } from '~/utils/utility';
 import { useSidemenuContext } from '~/hooks';
+import { useMediaQuery } from '@mantine/hooks';
 
 const Content = () => {
 	const { filterData, search } = useSidemenuContext();
+	const isMobile = useMediaQuery('(max-width: 36em)');
+
+	const sideMenuWidthClass = isMobile ? 'w-2/5' : 'w-1/5';
+
+	const contentWidthClass = isMobile ? 'w-3/5' : 'w-4/5';
 
 	return (
 		<div className='w-full h-full flex flex-row'>
-			<div className='w-1/5 h-full'>
+			<div className={`${sideMenuWidthClass} h-full`}>
 				<SideMenu />
 			</div>
-			<div className='w-4/5 h-full p-5'>
+
+			<div className={`${contentWidthClass} h-full p-5`}>
 				<List>
 					{filterData.map((item, index) => (
 						<List.Item key={index}>{highlightText(item, search)}</List.Item>
